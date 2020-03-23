8da3534fa504da57382c77232f603f4da062c944
Carlos Teller <carlos.z.teller@gmail.com>
2020-03-23 10:55:15 -0500

    Completed the Bob conversation exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Bob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bob: Hey buddy, how's it going? ");
+        String answer = input.nextLine();
+
+        if(answer.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if(answer.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if(answer.equals("")) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
+        }
+    }
 }
