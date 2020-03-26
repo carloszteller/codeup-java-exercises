f3501edf3e9e6365633bac2f73cfbee969ef8c69
Carlos Teller <carlos.z.teller@gmail.com>
2020-03-26 17:33:37 -0500

    Completed the ArraysExercises and ServerNameGenerator exercises

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class ArraysExercises {
+    public static void main(String[] args) {
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers)); // Without Arrays.toString() - prints: [I@38af3868
+
+        Person[] people = { new Person("Bob"), new Person("Jane"), new Person("John") };
+
+//        for(Person person : people) {
+//            System.out.println(person.getName());
+//        }
+
+        people = addPerson(people, new Person("Cindy"));
+
+        for(Person person : people) {
+            System.out.println(person.getName());
+        }
+    }
+
+    public static Person[] addPerson(Person[] people, Person person) {
+        Person[] addPeople = Arrays.copyOf(people, people.length + 1);
+        addPeople[addPeople.length - 1] = person;
+
+        return addPeople;
+    }
 }
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String[] adjs = { "attractive", "helpful", "icy", "nice", "obnoxious", "refined", "scrawny", "tinkling", "uneven", "yummy" };
+        String[] nouns = { "airline", "banana", "clock", "flowers", "hero", "light", "mountain", "photograph", "socks", "web" };
+
+        System.out.println("Here is your server name: " + randomString(adjs) + "-" + randomString(nouns));
+    }
+
+    public static String randomString(String[] a) {
+        int randomNum = (int) Math.floor(Math.random() * a.length);
+        String s = a[randomNum];
+
+        return s;
+    }
 }
