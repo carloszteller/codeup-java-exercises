51537cd024fdd376df0a06ccbad9aacf106252e3
Carlos Teller <carlos.z.teller@gmail.com>
2020-05-01 10:37:12 -0500

    Completed the warmup for 5/1/20

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,15 +1,27 @@
 public class Warmups {
     public static void main(String[] args) {
-        int[] numArray = { 10, 15, 3, 7 };
-        int k = 17;
+//        int[] numArray = { 10, 15, 3, 7 };
+//        int k = 17;
+//
+//        for(int i : numArray) {
+//            for(int j = 1; j < numArray.length; j++) {
+//                if(i + numArray[j] == k) {
+//                    System.out.println(i + " + " + numArray[j] + " = " + k);
+//                    break;
+//                }
+//            }
+//        }
 
-        for(int i : numArray) {
-            for(int j = 1; j < numArray.length; j++) {
-                if(i + numArray[j] == k) {
-                    System.out.println(i + " + " + numArray[j] + " = " + k);
-                    break;
-                }
-            }
+        System.out.println(isPalindrome("racecar"));
+    }
+
+    public static Boolean isPalindrome(String s) {
+        String reverse = "";
+
+        for(int i = s.length() - 1; i > 0; i--) {
+            reverse += s.charAt(i);
         }
+
+        return s.equalsIgnoreCase(reverse);
     }
 }
